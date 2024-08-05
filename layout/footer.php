58b614a18fc5b495c1ad2c4e17639314b39f84af
hendri <hendri240xie@gmail.com>
2024-08-05 11:30:44 +0700

    modify

--- layout/footer.php
+++ layout/footer.php
@@ -21,8 +21,6 @@
 <script src="app/plugins/chart.js/Chart.min.js"></script>
 <!-- time script -->
 <script src="assets/js/script.js"></script>
-<!-- script chart -->
-<script src="assets/js/script-ch.js"></script>
 <!-- boostrap 5 -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
